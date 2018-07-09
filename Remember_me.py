bbcc0f89b09ca6fae648a6ba93a9f815b4dce0d2
Unknown <mgijon94@gmail.com>
2018-07-09 22:46:40 +0200

    Comenzando un nuevo bot

---
+++ Remember_me.py
@@ -0,0 +1,39 @@
+import logging
+import sqlite3
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
+
+## LOGGING:
+## =======
+
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+## BOT:
+## ===
+
+la idea es crear un bot que nos recuerde mierdas varias
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
